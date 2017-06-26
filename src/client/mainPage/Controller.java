003b928e0054370820a96722ca086cbbf70c5fdc
ktr0328 <zf7heatp@gmail.com>
2017-06-26 09:40:50 +0900

    若干UIを変更

--- src/client/mainPage/Controller.java
+++ src/client/mainPage/Controller.java
@@ -6,7 +6,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,20 +18,21 @@ public class Controller implements Initializable {
     @FXML
     public ListView<Message> mainListView;
     public ListView<String> userListView;
+    public Button submit;
+    public TextArea messageArea;
 
-    private ObservableList<Message> observableList = FXCollections.observableArrayList();
+    private ObservableList<Message> messages = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        observableList.add(new Message("fromA", "name", "message1", "2017/06/20", 1));
         initializeMainListView();
 
-        mainListView.getItems().add(new Message("fromB", "あうあう", "message2", "2017/06/19", 1));
+        messages.add(new Message("fromB", "あうあう", "message2", "2017/06/19", 1));
         userListView.getItems().add("ユーザーリスト");
     }
 
     private void initializeMainListView() {
         mainListView.setCellFactory(e -> new MainCell());
-        mainListView.setItems(observableList);
+        mainListView.setItems(messages);
     }
 }
